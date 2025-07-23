dc1a78062840230411ae25dc7ef04940a3a6a1c8
Artag/Chris <scristxyz@gmail.com>
2025-07-23 15:07:46 -0500

    act

--- 01-creacionales/04-prototype.ts
+++ 01-creacionales/04-prototype.ts
@@ -9,3 +9,32 @@
  * 
  * https://refactoring.guru/es/design-patterns/prototype
  */
+
+class Document {
+    public title: string;
+    private content: string;
+    public author: string;
+
+    constructor(title: string, content: string, author: string) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+    dispalyInfo() {
+        console.log(`
+        Title: ${this.title}
+        Content: ${this.content}
+        Author: ${this.author}`)
+    }
+    clone(): Document {
+        return new Document(this.title, this.title, this.author)
+    }
+
+}
+
+function main() {
+    const document1 = new Document('cotizacion', '500 dolares', 'Christian')
+    console.log(document1)
+    document1.dispalyInfo()
+}
+main()
