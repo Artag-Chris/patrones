178f27c19ace5e3a4df0a2203ec4b30e463e10b9
Artag/Chris <scristxyz@gmail.com>
2025-07-30 14:26:56 -0500

    act tarea de facade

--- 02-estructurales/05.2-facade.ts
+++ 02-estructurales/05.2-facade.ts
@@ -56,20 +56,35 @@ class Memory {
 }
 
 // 2. Clase Facade - ComputerFacade
+interface ComputerFacadeProps{
+  cpu:CPU,
+  memory:Memory,
+  hardDrive:HardDrive
+}
 
 class ComputerFacade {
-  // TODO: Agregar los atributos necesarios CPU, Memory y HardDrive
 
-  // TODO: Agregar el constructor para instanciar los atributos CPU, Memory y HardDrive
-  constructor() {}
+  private cpu=new CPU();
+  private memory=new Memory();
+  private hardDrive=new HardDrive();
+
+ 
+  constructor({cpu,memory,hardDrive}:ComputerFacadeProps) {
+    this.cpu=cpu;
+    this.memory=memory;
+    this.hardDrive=hardDrive
+  }
 
   startComputer(): void {
     console.log('\n%cIniciando la computadora...', COLORS.cyan);
 
-    // TODO: ejecutar las operaciones necesarias para encender la computadora
+
     // 1. Cargar el sistema operativo en la memoria - memory.load(0, hardDrive.read(0, 1024))
+   this.memory.load(0,this.hardDrive.read(0, 1024))
     // 2. Saltar a la posición de memoria 0 - cpu.jump(0)
+    this.cpu.jump(0)
     // 3. Ejecutar las instrucciones del CPU - cpu.execute()
+    this.cpu.execute();
 
     console.log('Computadora lista para usar.\n');
   }
@@ -78,19 +93,24 @@ class ComputerFacade {
     console.log('\n%cApagando la computadora...', COLORS.red);
     console.log('Cerrando procesos y guardando datos...');
 
-    // TODO: ejecutar las operaciones necesarias para apagar la computadora
     // 1. Detener las operaciones del CPU - cpu.stopOperations()
+    this.cpu.stopOperations();
     // 2. Liberar la memoria - memory.free()
+    this.memory.free();
     // 3. Cerrar el disco duro - hardDrive.close()
+    this.hardDrive.close();
 
     console.log('Computadora apagada.\n');
   }
 }
 
 // 3. Código Cliente para Usar la Facade
-// TODO: Aquí no hay nada que hacer, debe de encender la computadora y apagarla sin problemas
+
 function main() {
-  const computer = new ComputerFacade();
+  const cpu =new CPU();
+  const memory=new Memory();
+  const hardDrive=new HardDrive();
+  const computer = new ComputerFacade({cpu,memory,hardDrive});
 
   // Encender la computadora usando la fachada
   computer.startComputer();
@@ -100,3 +120,5 @@ function main() {
 }
 
 main();
+
+      
