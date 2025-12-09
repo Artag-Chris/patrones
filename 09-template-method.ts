a75271c01ed6e62e755afccb06d9bd09736175ed
Artag/Chris <scristxyz@gmail.com>
2025-12-09 09:44:03 -0500

    act

--- 09-template-method.ts
+++ 09-template-method.ts
@@ -14,7 +14,7 @@
  * https://refactoring.guru/es/design-patterns/template-method
  */
 
-import { COLORS } from '../helpers/colors.ts';
+import { COLORS } from './helpers/colors.ts';
 
 /**
  * Contexto: Vamos a implementar un sistema que permite preparar
